3b2c35eb291792bfbaccbc0fb5813532b33c2547
TahaCE <taha1381asadi@gmail.com>
2024-01-03 19:49:52 +0330

    add Logs for modulating format of logs

--- src/main/java/Logs.java
+++ src/main/java/Logs.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class Logs {
+public class Logs {
+    private String date;
+    private String time;
+    private String componentName;
+    private String threadName;
+    private String type;
+    private String className;
+    private String message;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getComponentName() {
+        return componentName;
+    }
+
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    public void setThreadName(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
