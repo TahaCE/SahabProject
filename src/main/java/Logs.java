fb11198f8a3a1889bf78a2527c919e8d46cc9957
TahaCE <taha1381asadi@gmail.com>
2024-01-03 19:58:05 +0330

    Override toString in Logs

--- src/main/java/Logs.java
+++ src/main/java/Logs.java
@@ -62,4 +62,16 @@ public class Logs {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "date=" + date +
+                ", time=" + time +
+                ", threadName='" + threadName + '\'' +
+                ", type='" + type + '\'' +
+                ", className='" + className + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
